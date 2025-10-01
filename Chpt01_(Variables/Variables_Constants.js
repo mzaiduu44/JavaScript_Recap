50d398ca727543960f0766d9de58cc3095f8167e
Muhammad Zaid <mzaiduu44@gmail.com>
2025-10-01 14:36:57 +0500

    Setup project - Variables_&_Constants

---
+++ Chpt01_(Variables/Variables_Constants.js
@@ -0,0 +1,26 @@
+// Variables and Constants
+// JavaScript is a dynamically typed language, which means you don't need to declare the type of a variable when you create it.
+
+// Declaring a variable using 'let'
+let age = 25; // You can change the value of 'age' later
+console.log(age);
+
+//updating the valu of age
+age = 18;
+console.log(age);
+
+// Declaring a constant using 'const'
+const numberOfEyes = 2; // You cannot change the value of 'numberOfEyes'
+console.log(numberOfEyes);
+
+//Updating the value of numberOfEyes
+numberOfEyes = 3;
+console.log(numberOfEyes);
+
+// Declaring value through 'var'
+var Name = "Jinn";
+console.log(Name);
+
+// Updating the value of Name
+var Name = "Churail"; //It can also redeclare with the (Keyword) 'var'
+console.log(Name);
