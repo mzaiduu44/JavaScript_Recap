b6a4fd3ab23456ecdd39b07debb3b01c766ccadb
Muhammad Zaid <mzaiduu44@gmail.com>
2025-10-05 10:24:49 +0500

    DataTypes

---
+++ Chpt02_(Datatypes)/DataTypes.js
@@ -0,0 +1,21 @@
+//Datatype
+// There are two type of Datatype are...
+
+//...Primitive Datatypes.
+// String
+let myName = "Jinn";
+// Number: The range of number in (JS memory adress) is (2 is to the power of 53).
+let num = 123;
+//Boolen
+let isAllowed = false;
+// Null
+let noVal = null; //It's a datatype as well as also the stand alone value...
+//Undefined
+let unfefined = undefined; //The value hasn't defined yet...
+//Symbol
+let symbol = Symbol(); //To maintain privacy and serves as a secret
+
+//...Non-Primitives Datatypes
+let obj = {};
+
+console.log(typeof symbol);
